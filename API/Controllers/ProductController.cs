7bcda6586e4416ed809d0bc5e6f1f2926cf1a28e
Jalə Quliyeva <quliyevajale52@gmail.com>
2022-06-14 06:35:00 +0400

    Api

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOne(int id)
+        {
+                Product p=products
+        }
+    }
+}
